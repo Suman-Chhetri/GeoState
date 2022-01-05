69adc66cc3eef35caefdb9754ae2c84aa2923466
Diwash <mtnekros@gmail.com>
2022-01-05 12:40:42 +0545

    style: make featured cards smaller

--- src/components/MapContainer/FeaturedCard/index.vue
+++ src/components/MapContainer/FeaturedCard/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card" style="width: 18rem; margin: 2px;">
+  <div class="card" style="width: 14rem; margin: 2px;">
     <a :href="land.link" target="_blank">
       <img class="card-img-top" :src="land.img" alt="Card image cap">
     </a>
