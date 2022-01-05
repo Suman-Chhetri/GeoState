ce1642a4ba6f28fb3f1505b90439dcf78cb8fb70
Diwash <mtnekros@gmail.com>
2022-01-05 09:35:12 +0545

    feat: add plain bootstrap & jquery to project

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+import jQuery from 'jquery';
+import 'popper.js';
+import 'bootstrap';
+
+window.$ = window.jQuery = jQuery;
 
 createApp(App).use(store).use(router).mount('#app')
